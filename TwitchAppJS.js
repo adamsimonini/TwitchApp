cf4938324190c1df98340323ebee4e870bd22054
Adam Simonini <adam.simonini@gmail.com>
2017-04-06 16:58:57 -0400

    First Commit
    Added colors and general layout

---
+++ TwitchAppJS.js
@@ -0,0 +1,17 @@
+
+/* Roof for all Twitch.tv API requests
+https://api.twitch.tv/kraken/"channel"
+
+...callback=foo
+*/
+
+Twitch ClientID = "vrildt3l2ipx57q51hytkgthgzvyku";
+
+Secret = "b6osn0z5n5xq208ey6xoz51kynkus0";
+
+application/vnd.twitchtv[v3]+json
+
+  $.getJSON
+
+
+https://api.twitch.tv/kraken/streams?game=StarCraft+II%3A+Heart+of+the+Swarm&channel=test_channel,test_channel2
