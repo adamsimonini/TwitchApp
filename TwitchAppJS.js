eca54ac3e8bf601d3f753071c3b1449c73f58589
Adam Simonini <adam.simonini@gmail.com>
2017-04-07 11:34:41 -0400

    Functioning JSONP Request
    Extracting Data from JSONP. Beginning work of converying data in
    stylized form

--- TwitchAppJS.js
+++ TwitchAppJS.js
@@ -18,15 +18,48 @@ application/vnd.twitchtv[v3]+json
 https://api.twitch.tv/kraken/streams?game=StarCraft+II%3A+Heart+of+the+Swarm&channel=test_channel,test_channel2
 
 */
-$(document).on(ready(function(){
+$(document).ready(function(){
+  var channelName = "freecodecamp";
+
+    function APICall(){
+      $.ajax({
+        dataType: "jsonp",
+        url: 'https://wind-bow.gomix.me/twitch-api/channels/' + channelName + '?callback=?',
+        success: function(data){
+          console.log(data);
+          $("#logo").innerHTML = "<img src='https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png'>"
+          $("#test").append(data.game);
+          $("#test").append(data.status);
+          $("#test").append(data._id);
+          console.log(data.display_name);
+          console.log(data.status);
+          console.log(data._id);
+        }
+     });
+   }
+  APICall();
+});
+
+
+
+/*
+
+$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data){
+  var json = data;
+  var name = json[]
+});
+}
+
+
 
   function APICall(){
           $.ajax({
             type:"GET",
-            url: "https://api.twitch.tv/kraken/channels/nl_kripp"
+            url: "https://api.twitch.tv/kraken/streams/nl_kripp"
             dataType: "jsonp",
             success: function(data){
               console.log(data);
             }
             APICall();
 });
+*/
