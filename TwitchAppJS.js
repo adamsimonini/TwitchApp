c5a8abc37e4be3094cf551c99ed507423e61354b
Adam Simonini <adam.simonini@gmail.com>
2017-04-08 10:21:43 -0400

    Static Info
    Static information works. Not using dynamic settings at the moment, but
    rather pushing data onto premade, static div containers

--- TwitchAppJS.js
+++ TwitchAppJS.js
@@ -22,29 +22,32 @@ $(document).ready(function(){
 
 var master = document.getElementById("masterDiv");
 
-  function generateDivs(i, data){
+  function generateDivs(counter, data){
 
       var newDiv = document.createElement("div");
-      newDiv.id = "feed" + i.toString();
+      newDiv.id = "feed" + counter.toString();
       newDiv.class = "row text-center";
-      newDiv.innerText = data.display_name;
     //  newDiv.innerHTML = "<img style='width: 5%; height: 5%;' src='http://res.cloudinary.com/redkasa/image/upload/v1491615507/580b57fcd9996e24bc43c540_sfri3x.png'>";
+      newDiv.innerHTML = '<div class="row text-center"><div class="large-1 columns large-offset-3"><a id="logolink' + counter + '" href="" target="_blank"><img id="logo' + counter + '" class="channelLogo" src=""></a></div><div id="name' + counter + '" class="large-4 columns" style="border-style:solid;"></div><div class="large-1 columns small-offset-0"><img src="http://res.cloudinary.com/redkasa/image/upload/v1491615507/580b57fcd9996e24bc43c540_sfri3x.png"></div><div class="large-1 columns small-offset-0"></div></div><div class="row text-center"><div id="status' + counter + '" class="large-4 large-offset-4 columns" style="border-style:solid;"></div></div>';
+      document.getElementById("logo" + counter).src = data.logo;
+      document.getElementById("name" + counter).innerHTML = data.display_name;
+      document.getElementById("status" + counter).innerHTML = data.status;
+      document.getElementById("logolink" + counter).href = data.url;
       master.appendChild(newDiv);
+
   }
-  var prepopulated = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+  var prepopulated = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "faker", "pago3", "RobotCaleb", "noobs2ninjas"]
 
     function initialAPICall(){
-      for(var i=0;i<prepopulated.length;i++){
+      var counter = 0;
+      for (var i=0;i<prepopulated.length;i++){
         $.ajax({
           dataType: "jsonp",
           url: 'https://wind-bow.gomix.me/twitch-api/channels/' + prepopulated[i] + '?callback=?',
           success: function(data){
             console.log(data);
-            document.getElementById("logo1").src = data.logo;
-            document.getElementById("name1").innerHTML = data.display_name;
-            document.getElementById("status1").innerHTML = data.status;
-            document.getElementById("logolink1").href = data.url;
-            generateDivs(i, data);
+            counter++;
+            generateDivs(counter, data)
             }
           });
        }
